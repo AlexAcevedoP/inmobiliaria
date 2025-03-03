9f732c6ddcd1d521c7370aeaa0ff6049f3fb4351
Alex <conelalex89@gmail.com>
2025-03-03 10:56:48 -0500

    textos nososotros

--- views/paginas/nosotros.php
+++ views/paginas/nosotros.php
@@ -1,32 +1,39 @@
 <main class="contenedor seccion">
-        <h1>Conoce sobre Nosotros</h1>
+    <h1>Conoce sobre Nosotros</h1>
 
-        <div class="contenido-nosotros">
-            <div class="imagen">
-                <picture>
-                    <source srcset="build/img/nosotros.webp" type="image/webp">
-                    <source srcset="build/img/nosotros.jpg" type="image/jpeg">
-                    <img loading="lazy" src="build/img/nosotros.jpg" alt="Sobre Nosotros">
-                </picture>
-            </div>
-
-            <div class="texto-nosotros">
-                <blockquote>
-                    25 Años de experiencia
-                </blockquote>
-
-                <p>Proin consequat viverra sapien, malesuada tempor tortor feugiat vitae. In dictum felis et nunc aliquet molestie. Proin tristique commodo felis, sed auctor elit auctor pulvinar. Nunc porta, nibh quis convallis sollicitudin, arcu nisl semper mi, vitae sagittis lorem dolor non risus. Vivamus accumsan maximus est, eu mollis mi. Proin id nisl vel odio semper hendrerit. Nunc porta in justo finibus tempor. Suspendisse lobortis dolor quis elit suscipit molestie. Sed condimentum, erat at tempor finibus, urna nisi fermentum est, a dignissim nisi libero vel est. Donec et imperdiet augue. Curabitur malesuada sodales congue. Suspendisse potenti. Ut sit amet convallis nisi.</p>
-
-                <p>Aliquam lectus magna, luctus vel gravida nec, iaculis ut augue. Praesent ac enim lorem. Quisque ac dignissim sem, non condimentum orci. Morbi a iaculis neque, ac euismod felis. Fusce augue quam, fermentum sed turpis nec, hendrerit dapibus ante. Cras mattis laoreet nibh, quis tincidunt odio fermentum vel. Nulla facilisi.</p>
-            </div>
+    <div class="contenido-nosotros">
+        <div class="imagen" style="float: left; margin-right: 50px; width: 700px;">
+            <picture>
+                <source srcset="build/img/nosotros.webp" type="image/webp">
+                <source srcset="build/img/nosotros.jpg" type="image/jpeg">
+                <img loading="lazy" src="build/img/nosotros.jpg" alt="Sobre Nosotros" style="width: 100%; height: auto;">
+            </picture>
         </div>
-    </main>
 
-    <section class="contenedor seccion">
-        <h1>Más Sobre Nosotros</h1>
+        <div class="texto-nosotros">
+            
+    <p>En <span style=" font-weight: bold;"> Sanmuz Inmobiliaria,</span> somos tu aliado confiable en la compra y venta de inmuebles rurales, urbanos y parcelaciones. Ofrecemos servicios completos de licencias de construcción, diseño de planos arquitectónicos y estructurales, y construcción.</p>
 
-        <?php include 'iconos.php'; ?>
-    </section>
-    
+    <!-- hacer que los textos dentro de span sean en negrita  -->
+    <p><span style=" font-weight: bold;" >Evaluación de Propiedades:</span> Realizamos un análisis detallado del valor de mercado de tu propiedad, considerando factores clave como ubicación, estado y tendencias del mercado.</p>
+    <p><span style=" font-weight: bold;">Asesoramiento Legal:</span> Te guiamos en todos los aspectos legales de la compra y venta, incluyendo contratos, escrituras y normativas locales, para que tengas total tranquilidad.</p>
+    <p><span style=" font-weight: bold;">Marketing de Propiedades:</span> Utilizamos estrategias efectivas de marketing para promocionar tu propiedad. Desde fotografía profesional y visitas virtuales hasta anuncios en las mejores plataformas, nos aseguramos de que tu propiedad sea vista por los compradores adecuados.</p>
+    <p><span style=" font-weight: bold;">Negociación:</span> Actuamos como intermediarios en las negociaciones entre compradores y vendedores, buscando siempre el mejor acuerdo para ambas partes.</p>
+    <p><span style=" font-weight: bold;">Financiamiento:</span> Te asesoramos sobre las mejores opciones de financiamiento, incluyendo hipotecas y otros métodos de pago que se ajusten a tus necesidades.</p>
+    <p><span style=" font-weight: bold;">Inspecciones y Tasaciones:</span> Coordinamos inspecciones para evaluar el estado de las propiedades y proporcionamos recomendaciones de tasaciones.</p>
+    <p><span style=" font-weight: bold;">Asesoría en la Búsqueda de Propiedades:</span> Ayudamos a los compradores a encontrar el inmueble perfecto que se ajuste a sus necesidades y presupuesto.</p>
+    <p><span style=" font-weight: bold;">Acompañamiento en el Proceso de Cierre:</span> Te asistimos durante todo el proceso de cierre de la transacción, asegurándonos de que todos los documentos y requisitos estén en orden.</p>
+
+    <p>Esperamos poder ayudarte a encontrar el inmueble de tus sueños o vender tu propiedad al mejor precio. ¡Contáctanos hoy mismo para comenzar!</p>
+        </div>
+    </div>
+</main>
+
+<section class="contenedor seccion">
+    <h1>Más Sobre Nosotros</h1>
+
+    <?php include 'iconos.php'; ?>
+</section>
+
 <!-- Incluir el Botón de WhatsApp Flotante -->
 <?php include __DIR__ . '/../../includes/templates/whatsapp.php'; ?>
